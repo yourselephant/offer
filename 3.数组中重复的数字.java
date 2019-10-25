8e4715da4f5cc87c4fafb7f5237532a302fd1883
yourselephant <jdwd707@gmail.com>
2019-10-25 15:53:19 +0800

    3

---
+++ 3.数组中重复的数字.java
@@ -0,0 +1,21 @@
+public boolean duplicate(int[] nums,int length,int[] duplication){
+	if(nums == null || length<=0)
+		return false;
+	for(int i=0;i<length;i++){
+		while(nums[i]!=i){
+			if(nums[i] == nums[nums[i]]){
+
+				duplication[0] = nums[i];
+				return true;
+			}
+			swap(nums,i,nums[i]);
+		}	
+	}
+	return false;
+}
+
+private void swap(int[] nums,int i,int j){
+	int temp=nums[i];
+	nums[i] = nums[j];
+	nums[j] = temp;
+}
