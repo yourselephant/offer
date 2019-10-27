63614763df813467a4a2c5fd94cf93b1258b4b57
yourselephant <jdwd707@gmail.com>
2019-10-27 23:17:25 +0800

    4

---
+++ coding/4.二维数组中的查找.java
@@ -0,0 +1,15 @@
+public boolean Find(int target, int[][] matrix) {     
+	if (matrix == null || matrix.length == 0 || matrix[0].length == 0)         
+		return false;     
+	int rows = matrix.length, cols = matrix[0].length;     
+	int r = 0, c = cols - 1; // 从右上角开始     
+	while (r <= rows - 1 && c >= 0) {         
+		if (target == matrix[r][c])             
+			return true;         
+		else if(target > matrix[r][c])             
+			r++;         
+		else             
+			c--;     
+	}     
+	return false; 
+} 
