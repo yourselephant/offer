aba9fcc790d3222d474f189dc546fce3e3ad3015
yourselephant <jdwd707@gmail.com>
2019-11-02 21:25:47 +0800

    10.3

---
+++ coding/10.3.跳台阶.java
@@ -0,0 +1,12 @@
+public int JumpFloor(int n){
+	if(n<=2)
+		return n;
+	int pre2=1,pre1=2;
+	int result=0;
+	for(int i=2;i<n;i++){
+		result=pre1+pre2;
+		pre2=pre1;
+		pre1=result;
+	}
+	return result;
+}
