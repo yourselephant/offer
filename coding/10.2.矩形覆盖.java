279ac333c9e37685280ab4f8705fde1cc878067d
yourselephant <jdwd707@gmail.com>
2019-11-02 21:24:45 +0800

    10.2

---
+++ coding/10.2.矩形覆盖.java
@@ -0,0 +1,12 @@
+public int RectCover(int n){
+	if(n<=2)
+		return n;
+	int pre2=1,pre1=2;
+	int result=0;
+	for(int i=3;i<=n;i++){
+		result = pre2 +pre1;
+		pre2=pre1;
+		pre1=result;
+	}
+	return result;
+}
