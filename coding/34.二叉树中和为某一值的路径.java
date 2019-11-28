48f00a74b0158ab15144018ffbedfabe17f73bc1
yourselephant <jdwd707@gmail.com>
2019-11-28 22:53:49 +0800

    34

---
+++ coding/34.二叉树中和为某一值的路径.java
@@ -0,0 +1,20 @@
+private ArrayList<ArrayList<Integer> ret = new ArrayList<>();
+
+public ArrayList<ArrayList<Integer> FindPath(TreeNode root,int target){
+	backtracking(root,target,new ArrayList<>());
+	return ret;
+}
+
+private void backtracking(TreeNode node,int target,ArrayList<Integer> path){
+	if(root == null)
+		return;
+	path.add(node.val);
+	target -= node.val;
+	if(target == 0 && node.left == null && node.right == null){
+		ret.add(new ArrayList<>(path));
+	}else{
+		backtracking(node.left,target,path);
+		backtracking(node.right,target,path);
+	}
+	path.remove(path.size()-1);
+}
