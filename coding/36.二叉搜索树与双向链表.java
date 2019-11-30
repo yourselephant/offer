ec85f122341f22e1ba74e167e9a4246b6de51e4c
yourselephant <jdwd707@gmail.com>
2019-11-30 23:31:45 +0800

    36

---
+++ coding/36.二叉搜索树与双向链表.java
@@ -0,0 +1,20 @@
+private TreeNode pre = null;
+private TreeNode head = null;
+
+public TreeNode Convert(TreeNode root){
+	inOrder(root);
+	return head;
+}
+
+private void inOrder(TreeNode node){
+	if (node == null)
+        return;
+    inOrder(node.left);
+    node.left = pre;
+    if (pre != null)
+        pre.right = node;
+    pre = node;
+    if (head == null)
+        head = node;
+    inOrder(node.right);
+}
