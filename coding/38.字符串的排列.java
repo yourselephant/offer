0ad632e08c85488286a5d1cdf3afa94f11d8972f
yourselephant <jdwd707@gmail.com>
2019-12-01 22:31:26 +0800

    38

---
+++ coding/38.字符串的排列.java
@@ -0,0 +1,28 @@
+private ArrayList<String> ret = new ArrayList<>();
+
+public ArrayList<String> Permutation(String str){
+	if(str.length() == 0)
+		return ret;
+	char[] chars=str.toCharArray();
+	Arrays.sort(chars);
+	backtracking(chars,new boolean[chars.length],new StringBuilder());
+	return ret;
+}
+
+private void backtracking(char[] chars,boolean[] hasUsed,StringBuilder s){
+	if(s.length() == chars.length){
+		ret.add(s.toString());
+		return;
+	}
+	for(int i=0;i<chars.length;i++){
+		if(hasUsed[i])
+			continue;
+		if(i != 0 && chars[i]==chars[i-1] && !hasUsed[i-1])
+			continue;
+		hasUsed[i] = true;
+		s.append(chars[i]);
+		backtracking(chars,hasUsed,s);
+		s.deleteCharAt(s.length()-1);
+		hasUsed[i] = false;
+	}
+}
