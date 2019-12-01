2350868ec97ff96f9efdd17d800f46f4ae57973e
yourselephant <jdwd707@gmail.com>
2019-12-01 22:37:03 +0800

    39

---
+++ coding/39.数组中出现次数超过一半的数字.java
@@ -0,0 +1,16 @@
+public int MoreThanHalfNum_Solution(int[] nums){
+	int majority=num[0];
+	for(int i=1,cnt=1;i<nums.length;i++){
+		cnt = nums[i]==majority ? cnt+1 : cnt-1;
+		if(cnt == 0){
+			majority = nums[i];
+			cnt = 1;
+		}
+	}
+	int cnt = 0;
+	for(int val:nums)
+		if(val==majority){
+			cnt++;
+		}
+	return cnt > nums.length/2 ? majority : 0;
+}
