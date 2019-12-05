1e380747133435779e92010afee339ec7456f73a
yourselephant <jdwd707@gmail.com>
2019-12-05 23:15:12 +0800

    42

---
+++ coding/42.连续子数组的最大和.java
@@ -0,0 +1,11 @@
+public int FindGreatestSumOfSubArray(int[] nums){
+	if(nums == null || nums.length == 0)
+		return 0;
+	int greatestSum = Integer.MIN_VALUE;
+	int sum = 0;
+	for(int val:nums){
+		sum = sum<=0 ? val : sum+val;
+		greatrstSum = Math.max(greatestSum,sum);
+	}
+	return greatestSum;
+}
