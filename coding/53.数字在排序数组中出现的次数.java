c5f9908029cccbc9eef777b1c399b61a423195e5
yourselephant <jdwd707@gmail.com>
2019-12-13 23:30:38 +0800

    53

---
+++ coding/53.数字在排序数组中出现的次数.java
@@ -0,0 +1,17 @@
+public int GetNumberOfK(int[] nums, int K) {
+    int first = binarySearch(nums, K);
+    int last = binarySearch(nums, K + 1);
+    return (first == nums.length || nums[first] != K) ? 0 : last - first;
+}
+
+private int binarySearch(int[] nums, int K) {
+    int l = 0, h = nums.length;
+    while (l < h) {
+        int m = l + (h - l) / 2;
+        if (nums[m] >= K)
+            h = m;
+        else
+            l = m + 1;
+    }
+    return l;
+}
