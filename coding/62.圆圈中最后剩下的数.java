693f33694a47899c6734972e6192d405fccbc790
yourselephant <jdwd707@gmail.com>
2019-12-21 23:20:08 +0800

    62

---
+++ coding/62.圆圈中最后剩下的数.java
@@ -0,0 +1,7 @@
+public int LastRemaining_Solution(int n, int m) {
+    if (n == 0)     /* 特殊输入的处理 */
+        return -1;
+    if (n == 1)     /* 递归返回条件 */
+        return 0;
+    return (LastRemaining_Solution(n - 1, m) + m) % n;
+}
